e81db9f9e19c4f60a6367ebd45c56299a20b055e
Jonathan Sundquist <jsundquist@gmail.com>
2013-02-04 09:10:12 -0600

    Update web/index.php

--- web/index.php
+++ web/index.php
@@ -86,7 +86,7 @@ $app->get("/sessions/{id}", function (Silex\Application $app, $id) use ($navigat
 
 $app->get("/speakers", function (Silex\Application $app) use ($navigation) {
 
-    $sql = "SELECT fname, lname, bio FROM c4p WHERE status = 'accepted' GROUP BY fname, lname ORDER BY fname, last ASC";
+    $sql = "SELECT id, title, fname, lname, bio FROM c4p WHERE status = 'accepted' ORDER BY fname, last ASC";
 
     $speakers = $app['dbs']['mysql_read']->fetchAll($sql);
 
