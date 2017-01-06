93b3893bdf357a7d15ad8d5c4cf232af29737d24
Jonathan Sundquist <jsundquist@gmail.com>
2017-01-05 19:08:04 -0600

    Working on adding in our key note speakers

--- src/helpers/speaker-url.js
+++ src/helpers/speaker-url.js
@@ -1,4 +1,6 @@
 module.exports = function (options) {
     let speakerName = options.fn(this);
+    let pattern = /&#x27;/i
+    speakerName = speakerName.replace(pattern, '');
     return '/speakers/' + speakerName.split(' ').join('-').toLowerCase() + '.html';
 };
